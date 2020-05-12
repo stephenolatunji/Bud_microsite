e9ed322b9bd3148d4c8d3a6c5c6f62930fb47dbd
Stephen Olatunji <SOlatunji01@gcn.ab-inbev.com>
2020-05-12 02:33:58 +0100

    Integrated pdf download

--- src/Components/General/DownloadImage.js
+++ src/Components/General/DownloadImage.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { html2canvas } from "html2canvas";
-import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 import { Card, Col } from "react-bootstrap";
 
 const DownloadImage = ({ uploadedImageSrc, text }) => {
+
   const printDocument = () => {
     const input = document.getElementById("divToPrint");
     html2canvas(input).then((canvas) => {
       const imgData = canvas.toDataURL("image/png");
       const pdf = new jsPDF();
-      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.addImage(imgData, "PNG", 0, 0);
       // pdf.output('dataurlnewwindow');
       pdf.save("download.pdf");
     });
-  };
+  }
 
   return (
-    <div>
+    <div >
       <div className="mb5">
-        <button onClick={printDocument}>Print</button>
+        <button onClick={printDocument}>Download</button>
       </div>
 
       <div id="divToPrint">
